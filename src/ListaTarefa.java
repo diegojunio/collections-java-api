76ea21eb7098b82ec3c5ec8d2555e909c1d1e4fa
diegojunio <diegojunio@gmail.com>
2023-10-14 18:05:59 -0300

    Resolvendo desafio ListaTarefa

---
+++ src/ListaTarefa.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+
+        for(Tarefa t: tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalDeTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listatarefa = new ListaTarefa();
+        listatarefa.adicionarTarefa("Tarefa 1");
+        listatarefa.adicionarTarefa("Tarefa 2");
+        listatarefa.adicionarTarefa("Tarefa 3");
+
+        System.out.println("O numero total de elementos na lista é: " + listatarefa.obterNumeroTotalDeTarefas());
+        listatarefa.removerTarefa("Tarefa 2");
+        System.out.println("O numero total de elementos na lista é: " + listatarefa.obterNumeroTotalDeTarefas());
+        listatarefa.obterDescricoesTarefas();
+    }
+
+}
